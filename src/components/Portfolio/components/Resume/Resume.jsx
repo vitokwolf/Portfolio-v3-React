16ab2c2598a9fdf723f8e9e4bcffb1164f7c2cc8
Victor Lupascu <sinajeen@gmail.com>
2021-09-20 15:24:55 -0500

    add resume skeleton

--- src/components/Portfolio/components/Resume/Resume.jsx
+++ src/components/Portfolio/components/Resume/Resume.jsx
@@ -1,5 +1,24 @@
 function Resume() {
-  return <div>Resume</div>
+  return (
+    <div>
+      <p>Resume</p>
+      <a href="#">Download</a>
+      <ul>
+        <p>Front End</p>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+      </ul>
+      <ul>
+        <p>Back-end</p>
+        <li>Node</li>
+        <li>Express</li>
+        <li>MySql</li>
+        <li>Mongo</li>
+      </ul>
+    </div>
+  )
 }
 
 export default Resume
