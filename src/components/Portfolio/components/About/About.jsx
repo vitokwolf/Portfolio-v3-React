08dfe3f79f62e918833b5213dea6e7bfb96e2244
Victor Lupascu <sinajeen@gmail.com>
2021-09-20 12:18:17 -0500

    add contact form

--- src/components/Portfolio/components/About/About.jsx
+++ src/components/Portfolio/components/About/About.jsx
@@ -1,5 +1,15 @@
 function About() {
-  return <div>About</div>
+  return (
+    <div className="about">
+      <div className="avatar">Image</div>
+      <div className="bio">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+        commodi dolorum facilis error amet provident a fugit, quisquam dolores
+        nesciunt asperiores recusandae natus pariatur quae nobis alias cumque
+        mollitia quas.
+      </div>
+    </div>
+  )
 }
 
 export default About
--- src/components/Portfolio/components/Contact/Contact.jsx
+++ src/components/Portfolio/components/Contact/Contact.jsx
@@ -1,5 +1,28 @@
 function Contact() {
-  return <div>Contact</div>
+  return (
+    <div className="contact">
+      <form>
+        <label htmlFor="">
+          Name
+          <input placeholder="name" type="text" name="firstName" required />
+        </label>
+        <label htmlFor="">
+          Email{' '}
+          <input
+            placeholder="Email address"
+            type="email"
+            name="email"
+            required
+          />
+        </label>
+        <label htmlFor="">
+          Your Message{' '}
+          <input placeholder="message" type="text" name="message" required />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
 }
 
 export default Contact
